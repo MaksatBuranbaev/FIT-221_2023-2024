a9bf8dfed7737a6dd6962911e5815f9212b2bb8f
Nikolay Koren <nikolai.koren2004@gmail.com>
2023-09-21 23:59:26 +0600

    tests

---
+++ SpaceBattle.Lib.Tests/RotateCommandTest.cs
@@ -0,0 +1,63 @@
+using Moq;
+using TechTalk.SpecFlow;
+namespace SpaceBattle.Lib.Tests;
+
+delegate void LFunc();
+
+[Binding]
+public class RotateCommandTest
+{
+   private Mock<SpaceBattle.Lib.IRotateble> _rotatable = new Mock<IRotateble>();
+   private LFunc? _a;
+
+   [Given(@"космический корабль имеет угол наклона (.*) град к оси OX")]
+   public void ДанУголНаклона(int angle)
+   {
+      _rotatable.SetupGet(m => m.Position).Returns(new Angle(angle)).Verifiable();
+   }
+
+   [Given(@"космический корабль, угол наклона которого невозможно определить")]
+   public void УголНаклонаНевозможноОпределить()
+   {
+      _rotatable.SetupGet(m => m.Position).Throws<Exception>().Verifiable();
+   }
+
+   [Given(@"имеет мгновенную угловую скорость (.*) град")]
+   public void ДанаУгловаяСкорость(int rotateSpeed)
+   {
+      _rotatable.SetupGet(m => m.RotationalSpeed).Returns(new Angle(rotateSpeed)).Verifiable();
+   }
+
+   [Given(@"мгновенную угловую скорость невозможно определить")]
+   public void НевозможноОпределитьУгловуюСкорость()
+   {
+      _rotatable.SetupGet(m => m.RotationalSpeed).Throws<Exception>().Verifiable();
+   }
+
+   [Given(@"невозможно изменить угол наклона к оси OX космического корабля")]
+   public void НевозможноИзменитьУголНаклона()
+   {
+      _rotatable.SetupGet(m => m.Position).Throws<Exception>().Verifiable();
+      _rotatable.SetupGet(m => m.RotationalSpeed).Throws<Exception>().Verifiable();
+   }
+
+   [When(@"происходит вращение вокруг собственной оси")]
+   public void ПроисходитВращение()
+   {
+      _a = () => (new RotateCommand(_rotatable.Object)).Execute();
+   }
+
+   [Then(@"угол наклона космического корабля к оси OX составляет (.*) град")]
+   public void УголНаклонаСоставляет(int angle)
+   {
+      _a();
+      _rotatable.VerifySet(m => m.Position = new Angle(angle), Times.Once);
+   }
+
+   [Then(@"возникает ошибка Exception")]
+   public void ВозникаетОшибка()
+   {
+      Assert.Throws<Exception>(() => _a());
+   }
+
+}
---
+++ SpaceBattle.Lib/Angle.cs
@@ -0,0 +1,47 @@
+namespace SpaceBattle.Lib;
+
+public class Angle
+{
+    public int value { get; set; }
+    public int dim{get;}
+
+    public Angle(int value, int dim)
+    {
+        this.value = value % dim;
+        this.dim = dim;
+    }
+
+    public Angle(int angle)
+    {
+        this.value = (angle/360 * 8) % 8;
+        this.dim = 8;
+    }
+
+    public static Angle operator +(Angle ang1, Angle ang2)
+    {
+        if (ang1.dim != ang2.dim){
+            throw new Exception();
+        }
+        return new Angle(ang1.value + ang2.value, ang1.dim);
+    }
+
+    public static bool operator ==(Angle ang1, Angle ang2)
+    {
+        return (ang1.value == ang2.value && ang1.dim == ang2.dim);
+    }
+
+    public static bool operator !=(Angle ang1, Angle ang2)
+    {
+      return !(ang1 == ang2);
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return obj is Angle & this == (Angle) obj;
+    }
+
+    public override int GetHashCode()
+    {
+        return 0;
+    }
+}
