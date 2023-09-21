cb4b8779e83304f44aa54a554674a028fc8579db
Maksat <zenfonemaxprom124@gmail.com>
2023-09-21 20:22:59 +0600

    interface

---
+++ SpaceBattle.Lib/Interface.cs
@@ -0,0 +1,12 @@
+﻿namespace SpaceBattle.Lib;
+
+public interface ICommand 
+{
+	public void Execute();
+}
+
+public interface IMovable 
+{
+	public int[] Position {get; set;}
+	public int[] Velocity {get; set;}
+}
