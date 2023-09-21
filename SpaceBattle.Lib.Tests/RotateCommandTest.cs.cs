bd83e302754de6ddaea99c3d7763104536069571
Nikolay Koren <nikolai.koren2004@gmail.com>
2023-09-21 23:59:26 +0600

    tests

---
+++ SpaceBattle.Lib.Tests/RotateCommandTest.cs.cs
@@ -0,0 +1,11 @@
+using Moq;
+using TechTalk.SpecFlow;
+
+namespace SpaceBattle.Lib.Tests;
+
+public class MoveCommandTest
+{
+
+   
+   
+}
--- SpaceBattle.Lib/Rotate.cs
+++ SpaceBattle.Lib/Rotate.cs
@@ -9,7 +9,7 @@ public class Angle
         this.value = value;
     }
 
-    public static  operator +(Angle ang1, Angle ang2)
+    public static Angle operator +(Angle ang1, Angle ang2)
     {
         int angle = (ang1.value + ang2.value) % 360;
         return new Angle(angle);
