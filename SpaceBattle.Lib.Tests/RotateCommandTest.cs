ffd901db8ffac6ed6f10473e3c1cbc2cfd35f724
Nikolay Koren <nikolai.koren2004@gmail.com>
2023-10-21 16:41:04 +0600

    tests 0.1

--- SpaceBattle.Lib.Tests/RotateCommandTest.cs
+++ SpaceBattle.Lib.Tests/RotateCommandTest.cs
@@ -6,42 +6,43 @@ namespace SpaceBattle.Lib.Tests;
 public class RotateCommandTest
 {
    private Mock<SpaceBattle.Lib.IRotateble> rotateble = new Mock<IRotateble>();
-   private ICommand rotateCommand = new RotateCommand(rotateble.Object);
+   //private ICommand rotateCommand = new RotateCommand(rotateble.Object);
    private Exception actualException = new Exception();
 
-   [Given(@"Дано космический корабль имеет угол наклона (.*) град к оси OX")]
+   [Given(@"космический корабль имеет угол наклона (.*) град к оси OX")]
    public void ДанУголНаклона(int angle)
    {
       rotateble.SetupGet(m => m.Angle).Returns(new Angle(angle)).Verifiable();
    }
 
-   [Given(@"Дано космический корабль, угол наклона которого невозможно определить")]
+   [Given(@"космический корабль, угол наклона которого невозможно определить")]
    public void УголНаклонаНевозможноОпределить()
    {
       rotateble.SetupGet(m => m.Angle).Throws<Exception>().Verifiable();
    }
 
-   [Given(@"И имеет мгновенную угловую скорость (.*) град")]
+   [Given(@"имеет мгновенную угловую скорость (.*) град")]
    public void ДанаУгловаяСкорость(int rotateSpeed)
    {
       rotateble.SetupGet(m => m.RotationalSpeed).Returns(new Angle(rotateSpeed)).Verifiable();
    }
 
-   [Given(@"И мгновенную угловую скорость невозможно определить")]
+   [Given(@"мгновенную угловую скорость невозможно определить")]
    public void НевозможноОпределитьУгловуюСкорость()
    {
       rotateble.SetupGet(m => m.RotationalSpeed).Throws<Exception>().Verifiable();
    }
 
-   [Given(@"И невозможно изменить угол наклона к оси OX космического корабля")]
+   [Given(@"невозможно изменить угол наклона к оси OX космического корабля")]
    public void НевозможноИзменитьУголНаклона()
    {
       
    }
 
-   [When(@"Когда происходит вращение вокруг собственной оси")]
+   [When(@"происходит вращение вокруг собственной оси")]
    public void ПроисходитВращение()
    {
+      ICommand rotateCommand = new RotateCommand(rotateble.Object);
       try{
          rotateCommand.Execute();
       }
@@ -51,14 +52,14 @@ public class RotateCommandTest
       }
    }
 
-   [Then(@"Тогда угол наклона космического корабля к оси OX составляет (.*) град")]
+   [Then(@"угол наклона космического корабля к оси OX составляет (.*) град")]
    public void УголНаклонаСоставляет(int angle)
    {
       rotateble.VerifySet(m => m.Angle = new Angle(angle), Times.Once);
-      rotateble.VerifyAll();
+      //rotateble.VerifyAll();
    }
 
-   [Then(@"Тогда возникает ошибка Exception")]
+   [Then(@"возникает ошибка Exception")]
    public void ВозникаетОшибка()
    {
       Assert.ThrowsAsync<Exception>(() => throw actualException);
