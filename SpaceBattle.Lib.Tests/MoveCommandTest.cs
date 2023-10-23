f8170252e5b2f0ee833b2768ce2d413223f764f5
Maksat <zenfonemaxprom124@gmail.com>
2023-10-23 17:31:48 +0600

    MoveCommandTest_v2

--- SpaceBattle.Lib.Tests/MoveCommandTest.cs
+++ SpaceBattle.Lib.Tests/MoveCommandTest.cs
@@ -1,33 +1,62 @@
-﻿using Moq;
-
+﻿using System.Data;
+using Moq;
+using TechTalk.SpecFlow;
 namespace SpaceBattle.Lib.Tests;
 
+delegate void F();
+
+[Binding, Scope(Feature = "Прямолинейное движение")]
 public class MoveCommandTest
 {
+    private static Mock<IMovable> movable = new Mock<IMovable>();
+    private F? a;
+    [Given(@"космический корабль находится в точке пространства с координатами \((.*), (.*)\)")]
+         public void ДопустимКосмическийКорабльНаходитсяВТочкеПространстваСКоординатами(int p0, int p1)
+         {
+            movable.SetupGet(m => m.Position).Returns(new Vectorn(new int[] { p0, p1 })).Verifiable();
+         }
 
-    // 1. Всё работает
-    // 2. Не читается Position
-    // 3. Не читается Velocity
-    // 4. Не пишется в Position
+    [Given(@"изменить положение в пространстве космического корабля невозможно")]
+         public void ДопустимИзменитьПоложениеВПространствеКосмическогоКорабляНевозможно()
+         {
+            movable.SetupGet(m => m.Position).Throws<Exception>().Verifiable();
+         }
 
-    [Fact]
-    public void MoveCommandPositive()
-    {
-        // pre
-        var movable = new Mock<IMovable>();
+    [Given(@"космический корабль, положение в пространстве которого невозможно определить")]
+         public void ДопустимКосмическийКорабльПоложениеВПространствеКоторогоНевозможноОпределить()
+         {
+            movable.SetupGet(m => m.Position).Throws<Exception>().Verifiable();
+         }
 
-        movable.SetupGet(m => m.Position).Returns(new Vectorn(new int[] { 12, 5 })).Verifiable();
-        movable.SetupGet(m => m.Velocity).Returns(new Vectorn(new int[] { -7, 3 })).Verifiable();
+    [Given(@"имеет мгновенную скорость \((.*), (.*)\)")]
+         public void ДопустимИмеетМгновеннуюСкорость(int p0, int p1)
+         {
+            movable.SetupGet(m => m.Velocity).Returns(new Vectorn(new int[] { p0, p1 })).Verifiable();
+         }
 
-        ICommand moveCommand = new MoveCommand(movable.Object);
+    [Given(@"скорость корабля определить невозможно")]
+         public void ДопустимСкоростьКорабляОпределитьНевозможно()
+         {
+            movable.SetupGet(m => m.Velocity).Throws<Exception>().Verifiable();
+         }
 
+    [When(@"происходит прямолинейное равномерное движение без деформации")]
+         public void КогдаПроисходитПрямолинейноеРавномерноеДвижениеБезДеформации()
+         {
+            a = () => (new MoveCommand(movable.Object)).Execute();
+         }
 
-        // action
-        moveCommand.Execute();
-        
-        //post
-        // movable.Position is correct
-        movable.VerifySet(m => m.Position = new Vectorn(new int[] { 5, 8 }), Times.Once);
-        // movable.VerifyAll(); // !!!
-    }
+    [Then(@"возникает ошибка Exception")]
+         public void ТоВозникаетОшибкаException()
+         {
+            Assert.Throws<Exception>(() => a());
+         }
+
+    [Then(@"космический корабль перемещается в точку пространства с координатами \((.*), (.*)\)")]
+         public void ТоКосмическийКорабльПеремещаетсяВТочкуПространстваСКоординатами(int p0, int p1)
+         {
+            a();
+            movable.VerifySet(m => m.Position = new Vectorn(new int[] { p0, p1 }), Times.Once);
+         }
 }
+
