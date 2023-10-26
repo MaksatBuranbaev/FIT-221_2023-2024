4929f26355d4f7dcd15e329534b11aec886d2732
Maksat <zenfonemaxprom124@gmail.com>
2023-10-26 22:55:49 +0600

    vector test

--- SpaceBattle.Lib/Move.cs
+++ SpaceBattle.Lib/Move.cs
@@ -1,56 +1,9 @@
 namespace SpaceBattle.Lib;
-using System;
 
 public interface IMovable 
 {
-	public Vectorn Position {get; set;}
-	public Vectorn Velocity {get; }
-}
-
-
-public class Vectorn
-{
-    public int[] array { get; set; }
-    public Vectorn(int[] array)
-    {
-        this.array = array;
-    }
-
-    public static Vectorn operator +(Vectorn v1, Vectorn v2)
-    {
-        for(int i = 0; i < v1.array.Length; i++)
-        {
-            v1.array[i] += v2.array[i];
-        }
-        return v1;
-    }
-
-    public static bool operator ==(Vectorn v1, Vectorn v2)
-    {
-        for(int i = 0; i < v1.array.Length; i++)
-        {
-            if(v1.array[i] != v2.array[i])
-            {
-                return false;
-            }
-        }
-        return true;
-    }
-
-   public static bool operator !=(Vectorn v1, Vectorn v2)
-   {
-      return !(v1 == v2);
-   }
-
-    public override bool Equals(object? v1)
-    {
-        return v1 is Vectorn && this == (Vectorn)v1;
-    }
-
-    public override int GetHashCode()
-    {
-        return array.GetHashCode();
-    }
+	public Vector Position {get; set;}
+	public Vector Velocity {get; }
 }
 
 public class MoveCommand : ICommand
