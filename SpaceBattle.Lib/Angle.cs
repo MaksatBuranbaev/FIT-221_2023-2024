5ce1f55b1223b7c0f3ac5adcbb9ab9f62750947a
kiokroel <125490038+kiokroel@users.noreply.github.com>
2023-12-01 18:13:47 +0600

    Rotate (#3)
    * init_rotate
    
    * create Rotate
    
    * tests
    
    * .gitignore
    
    * ci
    
    * .
    
    * changing the constructor
    
    * added code formatting check
    
    * replacing the operator in angle.equals
    
    * corrections
    
    * corrections
    
    * corrections 2
    
    * yml
    
    * Angle.Equals
    
    ---------
    
    Co-authored-by: Maksat Buranbaev <zenfonemaxprom124@gmail.com>

---
+++ SpaceBattle.Lib/Angle.cs
@@ -0,0 +1,44 @@
+﻿namespace SpaceBattle.Lib;
+
+public class Angle
+{
+    public int Value { get; set; }
+    public int Dim { get; }
+
+    public Angle(int Value, int Dim)
+    {
+        this.Value = Value % Dim;
+        this.Dim = Dim;
+    }
+
+    public Angle(int Value)
+    {
+        this.Value = Value;
+        Dim = 8;
+    }
+
+    public static Angle operator +(Angle ang1, Angle ang2)
+    {
+        return new Angle(ang1.Value + ang2.Value, ang1.Dim);
+    }
+
+    public static bool operator ==(Angle ang1, Angle ang2)
+    {
+        return (ang1.Value == ang2.Value && ang1.Dim == ang2.Dim);
+    }
+
+    public static bool operator !=(Angle ang1, Angle ang2)
+    {
+        return !(ang1 == ang2);
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return obj is Angle angle && Value == angle.Value && Dim == angle.Dim;
+    }
+
+    public override int GetHashCode()
+    {
+        return new int[] { Value }.GetHashCode() + new int[] { Dim }.GetHashCode();
+    }
+}
