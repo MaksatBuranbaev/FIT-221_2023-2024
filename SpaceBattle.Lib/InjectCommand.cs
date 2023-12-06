dedd5bbd05890cb0c27efbd212a3456c10367fb8
Nikolay Koren <nikolai.koren2004@gmail.com>
2023-12-06 16:35:42 +0600

    create InjectCommand

---
+++ SpaceBattle.Lib/InjectCommand.cs
@@ -0,0 +1,21 @@
+﻿namespace SpaceBattle.Lib;
+
+public class InjectCommand : ICommand, IInjectableCommand
+{
+    private ICommand _cmd;
+
+    public InjectCommand(ICommand cmd)
+    {
+        _cmd = cmd;
+    }
+
+    public void Execute()
+    {
+        _cmd.Execute();
+    }
+
+    public void Inject(ICommand cmd)
+    {
+        _cmd = cmd
+    }
+}
