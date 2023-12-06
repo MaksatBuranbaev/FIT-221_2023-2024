1a4d89a827cdffb0c3fc53ebe9d332604b0a31b0
Maksat Buranbaev <125548791+MaksatBuranbaev@users.noreply.github.com>
2023-12-06 17:35:42 +0600

    Linear move (#5)
    * init
    
    * MoveCommandTest_v1
    
    * MoveCommandTest_v2
    
    * добавил yml
    
    * vector test
    
    * GetHashCode
    
    * VectorTest1.1
    
    * hash
    
    * gitignore
    
    * gitignore1.1
    
    * vector
    
    * vector1.2
    
    * vector1.3
    
    * format
    
    * fix
    
    * fixx
    
    * editconfig
    
    * сравнение через linq
    
    * сравнение через linq
    
    * пробелы
    
    * пробелы
    
    * пробел
    
    * изменил StepDefitions
    
    * StepDefinitions
    
    * StepDefinitions
    
    * yml
    
    * spacebattle
    
    * sp
    
    * format

---
+++ SpaceBattle.Lib/Vector.cs
@@ -0,0 +1,50 @@
+﻿namespace SpaceBattle.Lib;
+
+public class Vector
+{
+    public int[] array { get; set; }
+    public Vector(int[] array)
+    {
+        this.array = array;
+    }
+
+    public static Vector operator +(Vector v1, Vector v2)
+    {
+        if (v1.array.Length != v2.array.Length)
+        {
+            throw new Exception();
+        }
+
+        return new Vector(v1.array.Zip(v2.array, (x, y) => x + y).ToArray());
+    }
+
+    public static Vector operator -(Vector v1, Vector v2)
+    {
+        if (v1.array.Length != v2.array.Length)
+        {
+            throw new Exception();
+        }
+
+        return new Vector(v1.array.Zip(v2.array, (x, y) => x - y).ToArray());
+    }
+
+    public static bool operator ==(Vector v1, Vector v2)
+    {
+        return Enumerable.SequenceEqual(v1.array, v2.array);
+    }
+
+    public static bool operator !=(Vector v1, Vector v2)
+    {
+        return !(v1 == v2);
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return obj is Vector vector && this == vector;
+    }
+
+    public override int GetHashCode()
+    {
+        return array.GetHashCode();
+    }
+}
