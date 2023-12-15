dc0d9058b8c4e71f152f8c28bf4416dd5014835f
kiokroel <125490038+kiokroel@users.noreply.github.com>
2023-12-15 11:45:58 +0600

    Build tree (#7)
    * create IReader
    
    * IoC package reference
    
    * create CollisionTreeBuildCommand
    
    * tests CollisionTreeBuildCommand
    
    * correct tests CollisionTreeBuildCommand
    
    * create FileReader
    
    * test FileReader

---
+++ SpaceBattle.Lib.Tests/CollisionTree.Tests/FileReaderTests.cs
@@ -0,0 +1,37 @@
+﻿namespace SpaceBattle.Lib.Tests;
+
+public class FileReaderTests
+{
+    [Fact]
+    public void FileReadTest()
+    {
+        var arrays = new string[]{
+            "1 1 1",
+            "2 2 2",
+            "1 4 1",
+            "1 5 2",
+            "1 1 2"
+        };
+
+        var path = "FileReaderTest.txt";
+        File.WriteAllLines(path, arrays);
+
+        var reader = new FileReader(path);
+        var arraysFromFile = reader.Read();
+        var arraysExpected = new int[][]{
+            new int[]{1,1,1},
+            new int[]{2,2,2},
+            new int[]{1,4,1},
+            new int[]{1,5,2},
+            new int[]{1,1,2}
+        };
+
+        for (var i = 0; i < arraysExpected.Length; i++)
+        {
+            for (var j = 0; j < arraysExpected[i].Length; j++)
+            {
+                Assert.Equal(arraysExpected[i][j], arraysFromFile[i][j]);
+            }
+        }
+    }
+}
---
+++ SpaceBattle.Lib/СollisionTree/FileReader.cs
@@ -0,0 +1,17 @@
+﻿namespace SpaceBattle.Lib;
+
+public class FileReader : IReader
+{
+    private readonly string _path;
+    public FileReader(string path)
+    {
+        _path = path;
+    }
+    public int[][] Read()
+    {
+        var arrays = File.ReadAllLines(_path).Select(
+            line => line.Split(" ").Select(num => int.Parse(num)).ToArray()
+        ).ToArray();
+        return arrays;
+    }
+}
