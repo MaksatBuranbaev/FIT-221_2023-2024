04e5aae5dd6b050df221ceb0271859e55182856e
Nikolay Koren <nikolai.koren2004@gmail.com>
2023-12-17 18:35:46 +0600

    create MacroCommandStrategy

---
+++ SpaceBattle.Lib/MacroCommand/MacroCommandStrategy.cs
@@ -0,0 +1,16 @@
+﻿using Hwdtech;
+
+namespace SpaceBattle.Lib;
+
+public class MacroCommandStrategy : IStrategy
+{
+    public object Run(params object[] args)
+    {
+        var commands = new List<ICommand>();
+        args.ToList().ForEach(cmd =>
+        {
+            commands.Add(IoC.Resolve<ICommand>((string)cmd));
+        });
+        return IoC.Resolve<ICommand>("Command.Macro", commands.ToArray());
+    }
+}
