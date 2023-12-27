aaf0ab007db5fbc14561b45de0b60f5e9debc305
Nikolay Koren <nikolai.koren2004@gmail.com>
2023-12-28 02:28:11 +0600

    added another test for RegisterHandlerCommand

--- SpaceBattle.Lib.Tests/ExeptionHandler.Tests/RegisterHandlerCommandTests.cs
+++ SpaceBattle.Lib.Tests/ExeptionHandler.Tests/RegisterHandlerCommandTests.cs
@@ -7,7 +7,7 @@ namespace SpaceBattle.Lib.Tests;
 public class RegisterHandlerCommandTest
 {
     [Fact]
-    public void RegisterHandlerTest()
+    public void RegisterHandlerForOneTest()
     {
         new InitScopeBasedIoCImplementationCommand().Execute();
 
@@ -38,6 +38,55 @@ public class RegisterHandlerCommandTest
         Assert.Equal(
             (IExceptionHandler)((Dictionary<Type, object>)exceptionTree[cmd.Object.GetType()])[typeof(Exception)],
             handler.Object
+            );
+    }
+
+    [Fact]
+    public static void RegisterHandlerForTwoTest()
+    {
+        new InitScopeBasedIoCImplementationCommand().Execute();
+
+        IoC.Resolve<Hwdtech.ICommand>(
+            "Scopes.Current.Set",
+            IoC.Resolve<object>("Scopes.New", IoC.Resolve<object>("Scopes.Root"))
+        ).Execute();
+
+        var exceptionTree = new Dictionary<Type, object>();
+        IoC.Resolve<Hwdtech.ICommand>(
+            "IoC.Register",
+            "ExceptionHandler.Tree",
+            (object[] args) =>
+            {
+                return exceptionTree;
+            }
+        ).Execute();
+
+        var handler1 = new Mock<IExceptionHandler>();
+        var cmd1 = new Mock<ICommand>();
+
+        new RegisterHandlerCommand(cmd1.Object.GetType(), typeof(Exception), handler1.Object).Execute();
+
+        Assert.True(exceptionTree.ContainsKey(cmd1.Object.GetType()));
+        Assert.True(
+            ((Dictionary<Type, object>)exceptionTree[cmd1.Object.GetType()]).ContainsKey(typeof(Exception))
+            );
+        Assert.Equal(
+            (IExceptionHandler)((Dictionary<Type, object>)exceptionTree[cmd1.Object.GetType()])[typeof(Exception)],
+            handler1.Object
+            );
+
+        var handler2 = new Mock<IExceptionHandler>();
+        var cmd2 = new Mock<RotateCommand>();
+
+        new RegisterHandlerCommand(cmd2.Object.GetType(), typeof(Exception), handler2.Object).Execute();
+
+        Assert.True(exceptionTree.ContainsKey(cmd2.Object.GetType()));
+        Assert.True(
+            ((Dictionary<Type, object>)exceptionTree[cmd2.Object.GetType()]).ContainsKey(typeof(Exception))
+            );
+        Assert.Equal(
+            (IExceptionHandler)((Dictionary<Type, object>)exceptionTree[cmd2.Object.GetType()])[typeof(Exception)],
+            handler2.Object
             );
     }
 }
