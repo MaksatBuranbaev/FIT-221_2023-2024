d5abbfedc8c4d1e6f50f07020dc082ca2b8fc36b
Nikolay Koren <nikolai.koren2004@gmail.com>
2023-12-28 02:06:16 +0600

    create RegisterHandlerCommand

---
+++ SpaceBattle.Lib/ExceptionHandler/RegisterHandlerCommand.cs
@@ -0,0 +1,26 @@
+﻿using Hwdtech;
+
+namespace SpaceBattle.Lib;
+
+public class RegisterHandlerCommand : ICommand
+{
+    private readonly Type _cmdType;
+    private readonly Type _exceptionType;
+    private readonly IExceptionHandler _exceptionHandler;
+
+    public RegisterHandlerCommand(Type cmdType, Type exceptionType, IExceptionHandler exceptionHandler)
+    {
+        _cmdType = cmdType;
+        _exceptionType = exceptionType;
+        _exceptionHandler = exceptionHandler;
+    }
+
+    public void Execute()
+    {
+        var exceptionTree = IoC.Resolve<Dictionary<Type, object>>("ExceptionHandler.Tree");
+        exceptionTree[_cmdType] = exceptionTree.GetValueOrDefault(_cmdType, new Dictionary<Type, object>());
+
+        var node = (Dictionary<Type, object>)exceptionTree[_cmdType];
+        node[_exceptionType] = node.GetValueOrDefault(_exceptionType, _exceptionHandler);
+    }
+}
