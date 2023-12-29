349d3dafce3d1321940475d25249d25051aab0d6
Maksat Buranbaev <zenfonemaxprom124@gmail.com>
2023-12-30 00:34:45 +0600

    RepeatCommand

---
+++ SpaceBattle.Lib/MacroCommand/RepeatCommand.cs
@@ -0,0 +1,16 @@
+﻿namespace SpaceBattle.Lib;
+using Hwdtech;
+
+public class RepeatCommand : ICommand
+{
+    private readonly ICommand _cmd;
+
+    public RepeatCommand(ICommand cmd)
+    {
+        _cmd = cmd;
+    }
+    public void Execute()
+    {
+        IoC.Resolve<ICommand>("Queue.Add", _cmd).Execute();
+    }
+}
