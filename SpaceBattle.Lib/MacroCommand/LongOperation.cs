db8b57b0f4cb152fad29ece7601618b71b8ce52a
Maksat Buranbaev <zenfonemaxprom124@gmail.com>
2023-12-29 10:22:32 +0600

    LongOperation

---
+++ SpaceBattle.Lib/MacroCommand/LongOperation.cs
@@ -0,0 +1,17 @@
+using Hwdtech;
+
+namespace SpaceBattle.Lib;
+
+public class LongOperation : IStrategy
+{
+    public object Run(params object[] args)
+    {
+        var _name = (string) args[0];
+        var _obj = (IUObject) args[1];
+        var command = IoC.Resolve<ICommand>(_name, _obj);
+        var commandsList = new List<ICommand> { command };
+        var macroCommand = IoC.Resolve<ICommand>("Command.Macro", commandsList);
+        var longOperation = IoC.Resolve<ICommand>("Command.LongOperation", macroCommand, commandsList);
+        return longOperation;
+    }
+}
