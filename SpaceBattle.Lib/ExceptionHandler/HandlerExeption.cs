d07261ae94ab6b5710b75c4e99fe0100da45bce0
Maksat Buranbaev <zenfonemaxprom124@gmail.com>
2023-12-29 19:12:59 +0600

    HandlerExceptionStrategy 1.1

--- SpaceBattle.Lib/ExceptionHandler/HandlerExeption.cs
+++ SpaceBattle.Lib/ExceptionHandler/HandlerExeption.cs
@@ -1,59 +1,14 @@
-﻿/* using Hwdtech;
-
-namespace SpaceBattle.Lib
-{
-    public class HandlerException : IHandler
-    {
-        public ICommand SearchHandler(params Type[] args)
-        {
-            var _commandType = args[0];
-            var _exceptionType = args[1];
-
-            var keyCmd = _commandType.ToString();
-            var keyException = _exceptionType.ToString();
-
-            var tree = IoC.Resolve<Dictionary<string, IExceptionHandler>>("ExceptionHandler.Tree");
-
-            IExceptionHandler? handler;
-
-            if (tree.TryGetValue(keyCmd, out handler))
-            {
-                return (ICommand)handler;
-            }
-
-            else if (tree.TryGetValue(keyException, out handler))
-            {
-                return (ICommand)handler;
-            }
-
-            else if (tree.TryGetValue(keyCmd + keyException, out handler))
-            {
-                return (ICommand)handler;
-            }
-            else
-            {
-                //если нет обработчика для ошибок то получаем дефолтный для команды из айока
-                var handler = (ICommand)tree.GetValueOrDefault(_exceptionType, IoC.Resolve<ICommand>("Commands.Handler", _commandType));
-                return handler;
-            }
-        }
-    }
-} */
-
-namespace SpaceBattle.Lib;
+﻿namespace SpaceBattle.Lib;
 using Hwdtech;
 
 public class HandlerExceptionStrategy : IStrategy
 {
     public object Run(params object[] args)
     {
-        IExceptionHandler? handler;
+        IExceptionHandler handler;
 
-        var _commandType = args[0];
-        var _exceptionType = args[1];
-
-        var _keyCmd = _commandType.ToString();
-        var _keyException = _exceptionType.ToString();
+        var _keyCmd = args[0].ToString();
+        var _keyException = args[1].ToString();
 
         var tree = IoC.Resolve<Dictionary<string, IExceptionHandler>>("ExceptionHandler.Tree");
 
@@ -64,14 +19,12 @@ public class HandlerExceptionStrategy : IStrategy
 
         else if(tree.TryGetValue(_keyException, out handler))
         {
-            return handler;
+           return handler;
         }
 
-        else if(tree.TryGetValue(_keyCmd + _keyException, out handler))
+        else
         {
-            return handler;
+            return IoC.Resolve<IExceptionHandler>("DefaultExceptionHandler");
         }
-
-        return IoC.Resolve<IExceptionHandler>("DefaultExceptionHandler");
     }
 }
