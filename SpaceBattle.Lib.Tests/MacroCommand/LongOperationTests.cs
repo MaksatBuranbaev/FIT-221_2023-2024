2eef16ca100c2335e6e439eae9f8a967a2f7c1eb
Maksat Buranbaev <zenfonemaxprom124@gmail.com>
2023-12-31 14:12:57 +0600

    LongOperationTests 1.2

--- SpaceBattle.Lib.Tests/MacroCommand/LongOperationTests.cs
+++ SpaceBattle.Lib.Tests/MacroCommand/LongOperationTests.cs
@@ -1,4 +1,5 @@
 ﻿namespace SpaceBattle.Lib.Tests;
+
 using Hwdtech;
 using Hwdtech.Ioc;
 using Moq;
@@ -12,6 +13,27 @@ public class LongOperationTests
         IoC.Resolve<Hwdtech.ICommand>("Scopes.Current.Set",
             IoC.Resolve<object>("Scopes.New", IoC.Resolve<object>("Scopes.Root"))
         ).Execute();
+
+        var queue = new FakeQueue();
+        IoC.Resolve<Hwdtech.ICommand>(
+            "IoC.Register",
+            "Queue",
+            (object[] args) => queue
+        ).Execute();
+
+        IoC.Resolve<Hwdtech.ICommand>(
+            "IoC.Register",
+            "Queue.Add",
+            (object[] args) =>
+            {
+                var q = IoC.Resolve<FakeQueue>("Queue");
+                var cmd = (List<Lib.ICommand>)args[0];
+                var queuePusher = new Mock<Lib.ICommand>();
+                queuePusher.Setup(qp => qp.Execute()).Callback(new Action(() => q.Add(cmd[0])));
+
+                return queuePusher.Object;
+            }
+        ).Execute();
     }
 
     [Fact]
@@ -20,7 +42,7 @@ public class LongOperationTests
         var mockCommand = new Mock<Lib.ICommand>();
         mockCommand.Setup(x => x.Execute()).Verifiable();
 
-        var name = "Command.Movement";
+        var name = "Command.Move";
         var mockUObject = new Mock<IUObject>();
 
         IoC.Resolve<ICommand>("IoC.Register", name, (object[] args) => mockCommand.Object).Execute();
@@ -47,26 +69,11 @@ public class LongOperationTests
 
         var mockCommand = new Mock<Lib.ICommand>();
 
-        var queue = new FakeQueue();
-
-        IoC.Resolve<Hwdtech.ICommand>(
-            "IoC.Register",
-            "Queue.Add",
-            (object[] args) =>
-            {
-                var q = queue;
-                var cmd = (Lib.ICommand)args[0];
-                var queuePusher = new Mock<Lib.ICommand>();
-                queuePusher.Setup(qp => qp.Execute()).Callback(new Action(() => q.Add(cmd)));
-
-                return queuePusher.Object;
-            }
-        ).Execute();
-
-        var repeatCommand = new RepeatCommand(mockCommand.Object);
+        var repeatCommand = new RepeatCommand();
+        repeatCommand.Add(mockCommand.Object);
 
         repeatCommand.Execute();
 
-        Assert.Equal(mockCommand.Object, queue.Take());
+        Assert.Equal(mockCommand.Object, IoC.Resolve<FakeQueue>("Queue").Take());
     }
 }
