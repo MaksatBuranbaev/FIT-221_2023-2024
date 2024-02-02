b28f6b3d56f27fa70f864fb4d7ea755ecb4e5111
Maksat Buranbaev <zenfonemaxprom124@gmail.com>
2024-02-03 02:55:31 +0600

    ServerThread

---
+++ SpaceBattle.Lib/MultithreadedStatefulServer/ServerThread.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Concurrent;
+using Hwdtech;
+namespace SpaceBattle.Lib;
+
+public class ServerThread
+{
+    private readonly Thread _t;
+    private readonly BlockingCollection<ICommand> _q;
+    private bool stop = false;
+    private Action strategy;
+    public ServerThread(BlockingCollection<ICommand> q)
+    {
+        _q = q;
+        strategy = () =>
+        {
+            var cmd = _q.Take();
+            try
+            {
+                cmd.Execute();
+            }
+            catch (Exception e)
+            {
+                IoC.Resolve<ICommand>("Exception.Handle", cmd, e).Execute();
+            }
+        };
+        _t = new Thread(() =>
+        {
+            while (!stop)
+            {
+                strategy();
+            }
+        });
+    }
+    public void Start()
+    {
+        _t.Start();
+    }
+
+    public void Stop()
+    {
+        stop = true;
+    }
+    public void UpdateBehaviour(Action act)
+    {
+        strategy = act;
+    }
+    public void Add(ICommand cmd)
+    {
+        _q.Add(cmd);
+    }
+}
