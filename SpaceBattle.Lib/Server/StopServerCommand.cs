87ffeccd7342a8ea7d9902e2c2af7c85226d6927
Nikolay Koren <nikolai.koren2004@gmail.com>
2024-02-22 00:48:49 +0600

    create StopServerCommand

---
+++ SpaceBattle.Lib/Server/StopServerCommand.cs
@@ -0,0 +1,23 @@
+﻿using Hwdtech;
+
+namespace SpaceBattle.Lib;
+
+public class StopServerCommand : ICommand
+{
+    public void Execute()
+    {
+        Console.WriteLine("Остановка сервера...");
+
+        var idThreads = IoC.Resolve<Dictionary<int, object>>("Threads.Dictionary").Keys;
+
+        foreach (var id in idThreads)
+        {
+            IoC.Resolve<ICommand>("Soft Stop The Thread", id, () =>
+                {
+                    Console.WriteLine($"\t Остановлен поток {id}");
+                }).Execute();
+        }
+
+        Console.WriteLine("Все потоки остановлены");
+    }
+}
