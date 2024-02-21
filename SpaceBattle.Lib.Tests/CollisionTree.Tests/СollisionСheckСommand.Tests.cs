54de756b4fb64fd2e32a06b2e6a3c0b05e454da2
Maksat Buranbaev <125548791+MaksatBuranbaev@users.noreply.github.com>
2024-02-21 12:35:09 +0600

    Collision (#10)
    * СollisionСheckСommand
    
    * СollisionСheckСommand
    
    * СollisionСheckСommandTests
    
    * СollisionСheckСommand 1.1
    
    * СollisionСheckСommandTests 1.1
    
    * СollisionСheckСommand 1.2
    
    * СollisionСheckСommand 1.3
    
    * СollisionСheckСommandTests

---
+++ SpaceBattle.Lib.Tests/CollisionTree.Tests/СollisionСheckСommand.Tests.cs
@@ -0,0 +1,81 @@
+﻿using Hwdtech;
+using Hwdtech.Ioc;
+using Moq;
+namespace SpaceBattle.Lib.Tests;
+
+public class СollisionСheckСommandTests
+{
+    private static Vector? v1;
+    private static Vector? v2;
+    static СollisionСheckСommandTests()
+    {
+        new InitScopeBasedIoCImplementationCommand().Execute();
+
+        IoC.Resolve<Hwdtech.ICommand>(
+        "Scopes.Current.Set",
+        IoC.Resolve<object>("Scopes.New", IoC.Resolve<object>("Scopes.Root"))
+        ).Execute();
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "CollisionTree", (object[] args) =>
+        {
+            var tree = new Dictionary<int, object>()
+            {
+                { 0, new Dictionary<int, object>()},
+                { 1, new Dictionary<int, object>()},
+                { 2, new Dictionary<int, object>()},
+                { 3, new Dictionary<int, object>()}
+            };
+            ((Dictionary<int, object>)tree[0])[0] = new Dictionary<int, object>();
+            ((Dictionary<int, object>)((Dictionary<int, object>)tree[0])[0])[0] = new Dictionary<int, object>();
+            ((Dictionary<int, object>)((Dictionary<int, object>)((Dictionary<int, object>)tree[0])[0])[0])[0] = new Dictionary<int, object>();
+            return tree;
+        }).Execute();
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "DifferenceVector", (object[] args) =>
+        {
+            var p1 = (Vector)args[0];
+            var p2 = (Vector)args[1];
+            var v1 = (Vector)args[2];
+            var v2 = (Vector)args[3];
+            var vector = new List<int>{
+                p1.array[0] - p2.array[0],
+                p1.array[1] - p2.array[1],
+                v1.array[0] - v2.array[0],
+                v1.array[1] - v2.array[1],
+            };
+            return vector;
+        }).Execute();
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "UObject1TargetGetProperty", (object[] args) => v1).Execute();
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "UObject2TargetGetProperty", (object[] args) => v2).Execute();
+    }
+    [Fact]
+    public void СorrectСollisionСheckСommand()
+    {
+        var mockCommand = new Mock<Lib.ICommand>();
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Event.Collision", (object[] args) => mockCommand.Object).Execute();
+
+        v1 = new Vector(new int[] { 1, 1 });
+        v2 = new Vector(new int[] { 1, 1 });
+        var uOb1 = new Mock<IUObject>();
+        var uOb2 = new Mock<IUObject>();
+
+        var ccm = new СollisionСheckСommand(uOb1.Object, uOb2.Object);
+
+        ccm.Execute();
+        mockCommand.Verify();
+    }
+
+    [Fact]
+    public void IncorrectСollisionСheckСommand()
+    {
+        var mockCommand = new Mock<Lib.ICommand>();
+
+        v1 = new Vector(new int[] { 1, 1 });
+        v2 = new Vector(new int[] { 3, 4 });
+        var uOb1 = new Mock<IUObject>();
+        var uOb2 = new Mock<IUObject>();
+
+        var ccm = new СollisionСheckСommand(uOb1.Object, uOb2.Object);
+
+        ccm.Execute();
+        mockCommand.Verify(mc => mc.Execute(), Times.Never());
+    }
+}
