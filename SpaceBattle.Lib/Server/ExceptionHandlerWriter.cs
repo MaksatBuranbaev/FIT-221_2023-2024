9fa842722428bda5f5a42b5dd80ce65459752568
Nikolay Koren <nikolai.koren2004@gmail.com>
2024-02-22 00:49:32 +0600

    create ExceptionHandlerWriter

---
+++ SpaceBattle.Lib/Server/ExceptionHandlerWriter.cs
@@ -0,0 +1,21 @@
+﻿namespace SpaceBattle.Lib;
+
+public class ExceptionHandlerWriter : IExceptionHandler
+{
+    private readonly string _cmd;
+    private readonly string _path;
+    private readonly string _nameException;
+
+    public ExceptionHandlerWriter(string cmd, string path, string nameException)
+    {
+        _cmd = cmd;
+        _path = path;
+        _nameException = nameException;
+    }
+
+    public void Handle()
+    {
+        var data = (DateTime.Now).ToString();
+        File.AppendAllText(_path, $"{data} {_cmd} {_nameException}\n");
+    }
+}
