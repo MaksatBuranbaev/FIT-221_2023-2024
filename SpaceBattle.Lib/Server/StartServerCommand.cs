1b91d64b0ad57c35df105d26ce134ef387b1e734
Nikolay Koren <nikolai.koren2004@gmail.com>
2024-02-22 00:48:34 +0600

    create StartServerCommand

---
+++ SpaceBattle.Lib/Server/StartServerCommand.cs
@@ -0,0 +1,28 @@
+﻿using Hwdtech;
+
+namespace SpaceBattle.Lib;
+
+public class StartServerCommand : ICommand
+{
+    private readonly int _countThreads;
+    public StartServerCommand(int countThreads)
+    {
+        _countThreads = countThreads;
+    }
+
+    public void Execute()
+    {
+        Console.WriteLine("Запуск сервера...");
+
+        for (var id = 0; id < _countThreads; id++)
+        {
+            IoC.Resolve<ICommand>("Create And Start Thread", id, () =>
+                {
+                    Console.WriteLine($"\t Запущен поток {id}");
+                }).Execute();
+        }
+
+        Console.WriteLine($"Запущено {_countThreads} потоков");
+
+    }
+}
