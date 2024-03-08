b9034c65dc43d5b65ad031b27666e37e7a908bf9
Maksat <zenfonemaxprom124@gmail.com>
2024-03-08 20:01:53 +0600

    HardStopCommand

---
+++ SpaceBattle.Lib/StatefulServer/HardStopCommand.cs
@@ -0,0 +1,21 @@
+﻿namespace SpaceBattle.Lib;
+
+public class HardStopCommand : ICommand
+{
+    private readonly ServerThread _st;
+    public HardStopCommand(ServerThread st)
+    {
+        _st = st;
+    }
+    public void Execute()
+    {
+        if (_st.Equals(Thread.CurrentThread))
+        {
+            _st.Stop();
+        }
+        else
+        {
+            throw new Exception("Warning");
+        }
+    }
+}
