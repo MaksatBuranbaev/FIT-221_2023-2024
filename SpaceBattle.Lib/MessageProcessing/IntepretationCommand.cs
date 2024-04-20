cb00791d560841d0eca52c74a59427d6b235698d
Nikolay Koren <nikolai.koren2004@gmail.com>
2024-04-20 21:52:59 +0600

    create IntepretationCommand

---
+++ SpaceBattle.Lib/MessageProcessing/IntepretationCommand.cs
@@ -0,0 +1,19 @@
+﻿using Hwdtech;
+
+namespace SpaceBattle.Lib;
+
+public class InterpretationCommand : ICommand
+{
+    private readonly IMessage _message;
+
+    public InterpretationCommand(IMessage message)
+    {
+        _message = message;
+    }
+    public void Execute()
+    {
+        var gameId = _message.GameID;
+        var cmd = IoC.Resolve<ICommand>("CreateCommandStrategy", _message);
+        IoC.Resolve<ICommand>("Queue.Push", gameId, cmd).Execute();
+    }
+}
