486ee36ca49ade525c2c4aa19265778dc4141cc6
Nikolay Koren <nikolai.koren2004@gmail.com>
2024-04-20 21:53:14 +0600

    create CreateCommandStrategy

---
+++ SpaceBattle.Lib/MessageProcessing/CreateCommandStrategy.cs
@@ -0,0 +1,20 @@
+﻿using Hwdtech;
+
+namespace SpaceBattle.Lib;
+
+public class CreateCommandStrategy : IStrategy
+{
+    public object Run(params object[] args)
+    {
+        var message = (IMessage)args[0];
+
+        var commandName = message.TypeCommand;
+        var gameItemId = message.GameItemId;
+        var prop = message.Properties;
+
+        var uobj = IoC.Resolve<IUObject>("UObject.ById", gameItemId);
+        prop.ToList().ForEach(p => IoC.Resolve<ICommand>("UObject.SetProperty", uobj, p.Key, p.Value).Execute());
+
+        return IoC.Resolve<ICommand>("Command." + commandName, uobj);
+    }
+}
