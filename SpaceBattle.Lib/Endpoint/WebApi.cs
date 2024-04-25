951f2b02f2a2eee8552f3038c87dbb0198259c51
Maksat <zenfonemaxprom124@gmail.com>
2024-04-25 22:21:02 +0600

    WebApi

--- SpaceBattle.Lib/Endpoint/WebApi.cs
+++ SpaceBattle.Lib/Endpoint/WebApi.cs
@@ -1,11 +1,12 @@
-﻿using SpaceBattle.Lib;
+﻿using CoreWCF;
+using SpaceBattle.Lib;
 
 namespace WebHttp
 {
     [ServiceBehavior(IncludeExceptionDetailInFaults = true)]
     internal class WebApi : IWebApi
     {
-        public GameContract BodyEcho(GameContract param)
+        public GameContract BodyEcho(GameContract param) 
         {
             var endpoint = new Endpoint(param);
             endpoint.Execute();
