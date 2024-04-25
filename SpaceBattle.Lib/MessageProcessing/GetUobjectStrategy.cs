6c0535c919eb897601dfa183cafff278d0f3e76a
Nikolay Koren <nikolai.koren2004@gmail.com>
2024-04-25 23:24:46 +0600

    create GetUobjectStrategy

---
+++ SpaceBattle.Lib/MessageProcessing/GetUobjectStrategy.cs
@@ -0,0 +1,21 @@
+﻿namespace SpaceBattle.Lib;
+using Hwdtech;
+
+public class GetUObjectStrategy : IStrategy
+{
+    public object Run(params object[] args)
+    {
+        var uobjectId = (int)args[0];
+
+        IUObject? uobject;
+
+        var uobjects = IoC.Resolve<IDictionary<int, IUObject>>("UObject.Map");
+
+        if (uobjects.TryGetValue(uobjectId, out uobject))
+        {
+            return uobject;
+        }
+
+        throw new Exception();
+    }
+}
