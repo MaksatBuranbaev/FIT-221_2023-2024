4c593d21ad2fcfc78e04106769e9313ae308315b
Maksat <zenfonemaxprom124@gmail.com>
2024-04-25 22:13:39 +0600

    IWebApi

--- SpaceBattle.Lib/Endpoint/IWebApi.cs
+++ SpaceBattle.Lib/Endpoint/IWebApi.cs
@@ -1,16 +1,10 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-
-using System.Net;
+﻿using System.Net;
 using CoreWCF;
 using CoreWCF.OpenApi.Attributes;
 using CoreWCF.Web;
 
 namespace WebHttp
 {
-    // Note the OpenAPI attributes are not strictly necessary as defaults are chosen in most cases,
-    // but are used here to show how they can customize the swagger documentation.
-
     [ServiceContract]
     [OpenApiBasePath("/api")]
     internal interface IWebApi
@@ -18,7 +12,7 @@ namespace WebHttp
         [OperationContract]
         [WebInvoke(Method = "POST", UriTemplate = "/body")]
         [OpenApiTag("Tag")]
-        [OpenApiResponse(ContentTypes = new[] { "application/json", "text/xml" }, Description = "Success", StatusCode = HttpStatusCode.OK, Type = typeof(GameContract)) ]
+        [OpenApiResponse(ContentTypes = new[] { "application/json", "text/xml" }, Description = "Success", StatusCode = HttpStatusCode.OK, Type = typeof(GameContract))]
         GameContract BodyEcho(
             [OpenApiParameter(ContentTypes = new[] { "application/json", "text/xml" }, Description = "param description.")] GameContract param);
     }
