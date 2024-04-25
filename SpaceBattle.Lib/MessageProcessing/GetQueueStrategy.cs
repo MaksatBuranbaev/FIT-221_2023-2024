ceedc2b2a0a55591530c83ae2b184ef65f31412a
Nikolay Koren <nikolai.koren2004@gmail.com>
2024-04-25 23:24:29 +0600

    create GetQueueStrategy

---
+++ SpaceBattle.Lib/MessageProcessing/GetQueueStrategy.cs
@@ -0,0 +1,21 @@
+﻿namespace SpaceBattle.Lib;
+using Hwdtech;
+
+public class GetQueueStrategy : IStrategy
+{
+    public object Run(params object[] args)
+    {
+        var gameId = (int)args[0];
+
+        Queue<ICommand>? queue;
+
+        var queues = IoC.Resolve<IDictionary<int, Queue<ICommand>>>("Queue.Map");
+
+        if (queues.TryGetValue(gameId, out queue))
+        {
+            return queue;
+        }
+
+        throw new Exception();
+    }
+}
