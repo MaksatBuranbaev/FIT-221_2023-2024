4509b8d67b9d705838381a98bbdd266128d6f0f4
Maksat <zenfonemaxprom124@gmail.com>
2024-04-30 23:40:30 +0600

    ISpaceServer

---
+++ SpaceBattle.Lib/Endpoint/ISpaceServer.cs
@@ -0,0 +1,19 @@
+﻿using System.Net;
+using CoreWCF;
+using CoreWCF.OpenApi.Attributes;
+using CoreWCF.Web;
+
+namespace WebHttp
+{
+    [ServiceContract]
+    [OpenApiBasePath("/spacebattle")]
+    internal interface ISpaceServer
+    {
+        [OperationContract]
+        [WebInvoke(Method = "POST", UriTemplate = "/order")]
+        [OpenApiTag("spacebattle")]
+        [OpenApiResponse(ContentTypes = new[] { "application/json"}, Description = "Success", StatusCode = HttpStatusCode.OK, Type = typeof(GameContract))]
+        GameContract Order(
+            [OpenApiParameter(ContentTypes = new[] { "application/json"}, Description = "param description.")] GameContract param);
+    }
+}
