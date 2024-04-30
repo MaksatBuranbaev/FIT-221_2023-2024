eae50e80acc20375fe989ac9f5ae3138b619b993
Maksat <zenfonemaxprom124@gmail.com>
2024-05-01 00:50:44 +0600

    выделил сервер в отдельную папку

---
+++ SpaceBattle.Server/GameContract.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
+using CoreWCF.OpenApi.Attributes;
+
+namespace WebHttp
+{
+    [DataContract(Name = "GameContract")]
+    public class GameContract
+    {
+        [DataMember(Name = "type", Order = 1)]
+        [OpenApiProperty(Description = "command type")]
+        public string type { get; set; }
+
+        [DataMember(Name = "game_id", Order = 2)]
+        [OpenApiProperty(Description = "game_id")]
+        public string game_id { get; set; }
+
+        [DataMember(Name = "game_item_id", Order = 3)]
+        [OpenApiProperty(Description = "game_item_id")]
+        public int game_item_id { get; set; }
+
+        [DataMember(Name = "properties", Order = 4)]
+        [OpenApiProperty(Description = "other properties")]
+        public List<int> properties { get; set; }
+    }
+}
