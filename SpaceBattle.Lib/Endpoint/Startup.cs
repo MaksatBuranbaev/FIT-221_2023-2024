2fe0f76036deebce9b58eafc9334b47f84032c77
Maksat <zenfonemaxprom124@gmail.com>
2024-04-30 23:41:15 +0600

    Startup

--- SpaceBattle.Lib/Endpoint/Startup.cs
+++ SpaceBattle.Lib/Endpoint/Startup.cs
@@ -1,5 +1,4 @@
-﻿using System.Xml;
-using CoreWCF;
+﻿using CoreWCF;
 using CoreWCF.Configuration;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
@@ -15,12 +14,6 @@ internal sealed class Startup
             o.Title = "Test API";
             o.Version = "1";
             o.Description = "API Description";
-            o.TermsOfService = new("http://example.com/terms");
-            o.ContactName = "Contact";
-            o.ContactEmail = "support@example.com";
-            o.ContactUrl = new("http://example.com/contact");
-            o.ExternalDocumentUrl = new("http://example.com/doc.pdf");
-            o.ExternalDocumentDescription = "Documentation";
         });
 
         services.AddSingleton(new SwaggerOptions());
@@ -33,37 +26,12 @@ internal sealed class Startup
 
         app.UseServiceModel(builder =>
         {
-            var readerQuoates = new XmlDictionaryReaderQuotas
-            {
-                MaxBytesPerRead = 4096,
-                MaxDepth = 32,
-                MaxArrayLength = 16384,
-                MaxStringContentLength = 16384,
-                MaxNameTableCharCount = 16384
-            };
-
-            builder.AddService<WebApi>();
-            builder.AddServiceWebEndpoint<WebApi, IWebApi>(new WebHttpBinding
+            builder.AddService<SpaceServer>();
+            builder.AddServiceWebEndpoint<SpaceServer, ISpaceServer>(new WebHttpBinding
             {
                 MaxReceivedMessageSize = 5242880,
                 MaxBufferSize = 65536,
-                ReaderQuotas = readerQuoates
-            }, "api", behavior =>
-            {
-                behavior.HelpEnabled = true;
-                behavior.AutomaticFormatSelectionEnabled = true;
-            });
-
-            builder.AddServiceWebEndpoint<WebApi, IWebApi>(new WebHttpBinding
-            {
-                Security = new WebHttpSecurity
-                {
-                    Mode = WebHttpSecurityMode.Transport
-                },
-                MaxReceivedMessageSize = 5242880,
-                MaxBufferSize = 65536,
-                ReaderQuotas = readerQuoates
-            }, "api", behavior =>
+            }, "spacebattle", behavior =>
             {
                 behavior.HelpEnabled = true;
                 behavior.AutomaticFormatSelectionEnabled = true;
