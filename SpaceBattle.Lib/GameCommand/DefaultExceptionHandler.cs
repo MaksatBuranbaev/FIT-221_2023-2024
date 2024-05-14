2a3d22f544c2782c6d9fa309e06eeeaa7f585806
Maksat <zenfonemaxprom124@gmail.com>
2024-05-14 21:34:12 +0600

    add DefaultExceptionHandler

---
+++ SpaceBattle.Lib/GameCommand/DefaultExceptionHandler.cs
@@ -0,0 +1,14 @@
+using Hwdtech;
+namespace SpaceBattle.Lib;
+public class DefaultExceptionHandler: IExceptionHandler
+{
+    private readonly Exception _e;
+    public DefaultExceptionHandler(Exception e)
+    {
+        _e = e;
+    }
+    public void Handle()
+    {
+        throw _e;
+    }
+}
