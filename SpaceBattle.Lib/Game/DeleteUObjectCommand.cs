bc9fcc3e175e41204cb26614823f9065f4413146
Nikolay Koren <nikolai.koren2004@gmail.com>
2024-05-15 22:44:26 +0600

    create DeleteUObjectCommand

---
+++ SpaceBattle.Lib/Game/DeleteUObjectCommand.cs
@@ -0,0 +1,17 @@
+﻿namespace SpaceBattle.Lib;
+using Hwdtech;
+
+public class DeleteUObjectCommand : ICommand
+{
+    private readonly int _uObjectId;
+
+    public DeleteUObjectCommand(int uObjectId)
+    {
+        _uObjectId = uObjectId;
+    }
+
+    public void Execute()
+    {
+        IoC.Resolve<IDictionary<int, IUObject>>("UObject.Map").Remove(_uObjectId);
+    }
+}
