bbef91588014a4255611fdb7d9352a3b135b83d3
Nikolay Koren <nikolai.koren2004@gmail.com>
2024-05-15 22:44:16 +0600

    create DeleteGameCommand

---
+++ SpaceBattle.Lib/Game/DeleteGameCommand.cs
@@ -0,0 +1,23 @@
+﻿using Hwdtech;
+namespace SpaceBattle.Lib;
+
+public class DeleteGameCommand : ICommand
+{
+    private readonly int _gameId;
+
+    public DeleteGameCommand(int gameId)
+    {
+        _gameId = gameId;
+    }
+
+    public void Execute()
+    {
+        var games = IoC.Resolve<IDictionary<int, IInjectableCommand>>("Games.Map");
+        games[_gameId].Inject(
+            IoC.Resolve<ICommand>("Command.Empty")
+        );
+
+        var scopes = IoC.Resolve<IDictionary<int, object>>("Scope.Map");
+        scopes.Remove(_gameId);
+    }
+}
