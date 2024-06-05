ac18e750e18c2129ea1967ed0455b255a8f1ab46
Nikolay Koren <nikolai.koren2004@gmail.com>
2024-06-06 00:19:13 +0600

    create DependenciesInitCommand

---
+++ SpaceBattle.Lib/Initializing/DependenciesInitCommand.cs
@@ -0,0 +1,29 @@
+﻿namespace SpaceBattle.Lib;
+using Hwdtech;
+
+public class DependenciesInitCommand : ICommand
+{
+    public void Execute()
+    {
+        var dependencies = IoC.Resolve<IDictionary<string, IStrategy>>("Dependencies.Commands");
+
+        dependencies.ToList().ForEach(d =>
+        {
+            IoC.Resolve<Hwdtech.ICommand>(
+                "IoC.Register", 
+                "Command." + d.Key, 
+                (object[] args) => d.Value.Run(args)
+            ).Execute();
+        });
+        
+        IoC.Resolve<Hwdtech.ICommand>(
+            "IoC.Register",
+            "Command.Create",
+            (object[] args) => {
+                var type = (string)args[0];
+                var obj = args[1];
+                return dependencies[type].Run(new object[] { obj });
+            }
+        ).Execute();
+    }
+}
