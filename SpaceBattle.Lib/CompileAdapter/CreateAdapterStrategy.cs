435a112561464a96c1a391a388e0e1fc9011a030
Nikolay Koren <nikolai.koren2004@gmail.com>
2024-06-07 01:14:29 +0600

    create CreateAdapterStrategy

---
+++ SpaceBattle.Lib/CompileAdapter/CreateAdapterStrategy.cs
@@ -0,0 +1,23 @@
+﻿namespace SpaceBattle.Lib;
+using System.Reflection;
+using Hwdtech;
+
+public class CreateAdapterStrategy : IStrategy
+{
+    public object Run(params object[] args)
+    {
+        var uObject = (IUObject)args[0];
+        var uobjectType = uObject.GetType();
+        var targetType = (Type)args[1];
+
+        var map = IoC.Resolve<IDictionary<KeyValuePair<Type, Type>, Assembly>>("Adapter.Map");
+        var pair = new KeyValuePair<Type, Type>(uobjectType, targetType);
+
+        if (!map.ContainsKey(pair))
+        {
+            IoC.Resolve<ICommand>("Adapter.Compile", uobjectType, targetType).Execute();
+        }
+
+        return IoC.Resolve<object>("Adapter.Find", uObject, targetType);
+    }
+}
