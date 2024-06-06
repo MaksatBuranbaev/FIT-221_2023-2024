804c78a22b1e7536df24f7ded3a98298b30531ef
Maksat <zenfonemaxprom124@gmail.com>
2024-06-07 01:24:05 +0600

    add ArrangeShipsCommand

---
+++ SpaceBattle.Lib/Initializing/ArrangeShipsCommand.cs
@@ -0,0 +1,16 @@
+namespace SpaceBattle.Lib;
+using Hwdtech;
+public class ArrangeShipsCommand: ICommand
+{
+    private IUObject? _uObject;
+    public void Execute()
+    {
+        var shipsArrangeIterator = IoC.Resolve<ShipsArrangeIterator>("Game.ShipsArrangeIterator");
+
+        foreach(var position in shipsArrangeIterator)
+        {
+            _uObject = IoC.Resolve<IUObject>("Game.UObject.Arrange");
+            IoC.Resolve<ICommand>("Game.Arrange.Ship", _uObject, position).Execute();
+        }
+    }
+}
